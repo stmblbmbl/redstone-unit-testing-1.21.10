cd8c9d1072712be3518a0640b8ad0963502cf8d4
Jonah Collins <jonahcllns@proton.me>
2026-03-04 00:05:14 +0000

    Initial Commit

---
+++ src/client/java/rs_unit_test/RedstoneUnitTestingClient.java
@@ -0,0 +1,10 @@
+package rs_unit_test;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RedstoneUnitTestingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
