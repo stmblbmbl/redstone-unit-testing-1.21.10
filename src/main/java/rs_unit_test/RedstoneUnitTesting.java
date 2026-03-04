7e0211b38be24db0ff1c36604771a7c9d4bb8d9a
Jonah Collins <jonahcllns@proton.me>
2026-03-04 00:33:52 +0000

    We can now run something every tick

--- src/main/java/rs_unit_test/RedstoneUnitTesting.java
+++ src/main/java/rs_unit_test/RedstoneUnitTesting.java
@@ -2,6 +2,11 @@ package rs_unit_test;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,5 +25,12 @@ public class RedstoneUnitTesting implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		// Register what I want to happen every tick
+		ServerTickEvents.START_SERVER_TICK.register(server -> {
+			ServerWorld overworld = server.getOverworld();
+			BlockPos position = new BlockPos(0, 64, 0);
+			BlockState blockstate = Blocks.REDSTONE_ORE.getDefaultState();
+			overworld.setBlockState(position, blockstate, 2);
+		});
 	}
 }
--- src/main/java/rs_unit_test/mixin/ServerMixin.java
+++ src/main/java/rs_unit_test/mixin/ServerMixin.java
@@ -1,15 +1,23 @@
 package rs_unit_test.mixin;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
-public class ExampleMixin {
+public class ServerMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+	}
+	// Run at end of loadWorld
+	@Inject(at = @At("RETURN"), method = "loadWorld")
+	private void init_return(CallbackInfo info) {
+		// This code is injected into the end of MinecraftServer.loadWorld()
 	}
 }
